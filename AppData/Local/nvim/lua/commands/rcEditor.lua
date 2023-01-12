8697173472d7da6561b5f7f2c2e65e0450d9a4d6
AsPulse <aspulse.x@gmail.com>
2023-01-12 20:43:06 +0900

    Add: gorepo command

--- AppData/Local/nvim/lua/commands/rcEditor.lua
+++ AppData/Local/nvim/lua/commands/rcEditor.lua
@@ -5,6 +5,26 @@ vim.api.nvim_create_user_command(
 )
 
 vim.api.nvim_create_user_command(
+  'Gorepo',
+  function(opts)
+    vim.api.nvim_command('silent! cd $GHDEV/' .. opts.fargs[1] .. ' | silent! Fern .')
+  end,
+  {
+    nargs = 1,
+    complete = function(arg_lead, _, _)
+      local complete_list = {}
+      local fd = vim.loop.fs_scandir(vim.env['GHDEV'])
+      for name in function() return vim.loop.fs_scandir_next(fd) end do
+        if vim.startswith(name, arg_lead) then
+          table.insert(complete_list, name)
+        end
+      end
+      return complete_list;
+    end
+  }
+)
+
+vim.api.nvim_create_user_command(
   'Reloadrc',
   function()
     vim.api.nvim_command('!chezmoi apply')
