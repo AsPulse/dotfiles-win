b046eb9d991c00ac92af90a37fa1dcbc04b489ae
AsPulse <aspulse.x@gmail.com>
2023-01-15 09:32:58 +0900

    Add(lexima): HTML Tag Indent

--- AppData/Local/nvim/lua/plugin-settings/lexima.lua
+++ AppData/Local/nvim/lua/plugin-settings/lexima.lua
@@ -1,20 +1,40 @@
+
+function LeximaRulesReset()
+  vim.fn['lexima#set_default_rules']()
+  vim.fn['lexima#add_rule']({
+     filetype = { 'typescript', 'typescriptreact' },
+     priority = 2,
+     char = '<cr>',
+     delete = 1,
+     at = [[css`\%#`]],
+     input = '<cr>  ',
+     input_after = '<cr>`;'
+  })
+  vim.fn['lexima#add_rule']({
+     filetype = { 'typescriptreact', 'html' },
+     priority = 2,
+     char = '<cr>',
+     at = [[<[a-zA-Z]*>\%#<\/[a-zA-Z]*>]],
+     input = '<cr>  ',
+     input_after = '<cr>'
+  })
+  vim.api.nvim_set_var('isLeximaLoaded', true);
+end
+
 local modules = {
   {
     'cohama/lexima.vim',
-    config = function()
-      vim.fn['lexima#add_rule']({
-         filetype = { 'typescript', 'typescriptreact' },
-         priority = 2,
-         char = '<cr>',
-         delete = 1,
-         at = [[css`\%#`]],
-         input = '<cr>  ',
-         input_after = '<cr>`;'
-      })
-    end
+    config = LeximaRulesReset
   }
 }
 
+if vim.g.isLeximaLoaded == nil then
+  vim.api.nvim_set_var('isLeximaLoaded', false)
+end
 
+if vim.api.nvim_get_var('isLeximaLoaded') then
+  LeximaRulesReset()
+  require('notify')('Lexima Rules Reloaded!', 'info', { timeout = 500, render = 'minimal', stages = 'fade' })
+end
 
 return modules
