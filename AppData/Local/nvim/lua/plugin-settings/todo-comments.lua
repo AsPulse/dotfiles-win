ba2ba48ad8fb7bd7c1fa548cbfe2482598f1ead8
AsPulse <aspulse.x@gmail.com>
2023-01-22 11:08:29 +0900

    Fix: unnecessary comment

--- AppData/Local/nvim/lua/plugin-settings/todo-comments.lua
+++ AppData/Local/nvim/lua/plugin-settings/todo-comments.lua
@@ -2,11 +2,7 @@ return {
   "folke/todo-comments.nvim",
   requires = "nvim-lua/plenary.nvim",
   config = function()
-    require("todo-comments").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
+    require("todo-comments").setup {}
   end
 }
 
