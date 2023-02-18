60c68f1e57117bb26bff2f9aff8712e47f0ac2b7
AsPulse <aspulse.x@gmail.com>
2023-02-18 17:08:04 +0900

    Add: Install lazy.nvim procedure

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -1,6 +1,23 @@
 local required = require('util.required')
 
+-- Load Configs
 required.under('configs')
+
+-- Install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup Plugins
 vim.api.nvim_set_var('jetpack_copy_method', 'copy');
 vim.cmd('packadd vim-jetpack')
 local plugins = {{ 'tani/vim-jetpack', opt = 1 }};
@@ -12,6 +29,8 @@ for _, exported in ipairs(pluginSettings) do
 end
 require('jetpack.packer').add(plugins)
 
+
+-- Load Commands
 required.under('commands')
 
 vim.g.isReload = true;
