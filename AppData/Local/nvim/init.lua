bb4ee9d8cfb1033d2fd03dafc778aa0a8ec61848
AsPulse <aspulse.x@gmail.com>
2023-02-18 17:13:19 +0900

    Migrate to lazygit

--- AppData/Local/nvim/init.lua
+++ AppData/Local/nvim/init.lua
@@ -18,17 +18,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup Plugins
-vim.api.nvim_set_var('jetpack_copy_method', 'copy');
-vim.cmd('packadd vim-jetpack')
-local plugins = {{ 'tani/vim-jetpack', opt = 1 }};
 local pluginSettings = required.under('plugin-settings')
-for _, exported in ipairs(pluginSettings) do
-  for _, plugin in ipairs(exported) do
-    table.insert(plugins, plugin)
-  end
-end
-require('jetpack.packer').add(plugins)
-
+require('lazy').setup(pluginSettings)
 
 -- Load Commands
 required.under('commands')
