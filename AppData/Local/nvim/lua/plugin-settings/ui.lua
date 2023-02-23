c29b4af59c439fc4199364e4e939e62b7d33a251
AsPulse <aspulse.x@gmail.com>
2023-02-23 11:26:01 +0900

    Add: vim-smoothie

--- AppData/Local/nvim/lua/plugin-settings/ui.lua
+++ AppData/Local/nvim/lua/plugin-settings/ui.lua
@@ -21,13 +21,6 @@ return {
     end
   },
   {
-    'lewis6991/satellite.nvim',
-    event = 'BufEnter *.*',
-    config = function ()
-      require('satellite').setup()
-    end
-  },
-  {
     'lewis6991/gitsigns.nvim',
     event = 'BufEnter *.*',
     config = function ()
