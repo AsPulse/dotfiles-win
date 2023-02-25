c58b088e4e2424fbbf20c27677f3f77961d31044
AsPulse <aspulse.x@gmail.com>
2023-02-25 11:53:25 +0900

    Add: prevent launch eslint where under deno repo

--- AppData/Local/nvim/lua/plugin-settings/lsp.lua
+++ AppData/Local/nvim/lua/plugin-settings/lsp.lua
@@ -30,11 +30,14 @@ return {
       })
 
       local null_ls = require('null-ls')
+      local condition = function(utiles)
+        return utils.root_has_file('package.json')
+      end
       null_ls.setup({
         sources = {
-          null_ls.builtins.code_actions.eslint_d,
-          null_ls.builtins.diagnostics.eslint_d,
-          null_ls.builtins.formatting.eslint_d,
+          null_ls.builtins.code_actions.eslint_d.with({ condition = condition }),
+          null_ls.builtins.diagnostics.eslint_d.with({ condition = condition }),
+          null_ls.builtins.formatting.eslint_d.with({ condition = condition }),
           null_ls.builtins.diagnostics.editorconfig_checker
         }
       })
