07fd54547a079031feca7ccf88dcdc4ad9953556
AsPulse <aspulse.x@gmail.com>
2023-02-27 14:20:38 +0900

    Add: more 2 terminals to my vim

--- AppData/Local/nvim/lua/plugin-settings/terminal.lua
+++ AppData/Local/nvim/lua/plugin-settings/terminal.lua
@@ -1,11 +1,74 @@
-function _Lazygit_toggle()
-  require('toggleterm.terminal').Terminal:new({
-    cmd = 'lazygit',
-    dir = vim.fn.getcwd(),
-    direction = 'float',
-    hidden = true,
-    count = 1
-  }):toggle()
+local _terminal_worker = nil
+local _terminal_worker_cwd = nil
+
+local _terminal_server = nil
+
+local _terminal_lazygit = nil
+local _terminal_lazygit_cwd = nil
+
+local function _terminal_worker_open()
+  if _terminal_worker == nil then
+    _terminal_worker = require('toggleterm.terminal').Terminal:new({
+      cmd = 'powershell',
+      dir = vim.fn.getcwd(),
+      direction = 'float',
+      hidden = true,
+      count = 2,
+    })
+    _terminal_worker_cwd = vim.fn.getcwd()
+  end
+  if _terminal_worker_cwd ~= vim.fn.getcwd() then
+    _terminal_worker_cwd = vim.fn.getcwd()
+    _terminal_worker:change_dir(_terminal_worker_cwd)
+  end
+  _terminal_worker:toggle()
+end
+
+local function _terminal_lazygit_open()
+  if _terminal_lazygit ~= nil and _terminal_lazygit_cwd ~= vim.fn.getcwd() then
+    _terminal_lazygit_cwd = vim.fn.getcwd()
+    _terminal_lazygit:shutdown()
+    _terminal_lazygit = nil
+  end
+  if _terminal_lazygit == nil then
+    _terminal_lazygit = require('toggleterm.terminal').Terminal:new({
+      cmd = 'lazygit',
+      dir = vim.fn.getcwd(),
+      direction = 'float',
+      hidden = true,
+      count = 1,
+    })
+    _terminal_lazygit_cwd = vim.fn.getcwd()
+  end
+  _terminal_lazygit:open()
+end
+
+local function _terminal_server_open()
+  if _terminal_server == nil then
+    _terminal_server = require('toggleterm.terminal').Terminal:new({
+      cmd = 'powershell',
+      dir = vim.fn.getcwd(),
+      direction = 'float',
+      hidden = true,
+      count = 3,
+      on_close = function()
+        _terminal_server = nil
+      end,
+    })
+  end
+  _terminal_server:open()
+end
+
+local function _terminal_close()
+  if _terminal_worker ~= nil then
+    _terminal_worker:close()
+  end
+  if _terminal_lazygit ~= nil then
+    _terminal_lazygit:close()
+  end
+  if _terminal_server ~= nil then
+    _terminal_server:close()
+  end
 end
 
 return {
@@ -25,9 +88,18 @@ return {
       for option, value in pairs(powershell_options) do
         vim.opt[option] = value
       end
-      vim.api.nvim_set_keymap('n', 'git', '<cmd>lua _Lazygit_toggle()<cr>', { noremap = true, silent = true })
-      vim.api.nvim_set_keymap('t', '<leader>q', '<C-\\><C-n>', { noremap = true, silent = true })
-    end
+      vim.keymap.set('n', '<leader>git', _terminal_lazygit_open, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>termw', _terminal_worker_open, { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>terms', _terminal_server_open, { noremap = true, silent = true })
+      vim.keymap.set('t', '<leader>n', '<C-\\><C-n>', { noremap = true, silent = true })
+      vim.keymap.set('t', '<leader>q', _terminal_close, { noremap = true, silent = true })
+    end,
+    config = function()
+      require('toggleterm').setup({
+        start_in_insert = true,
+        persist_mode = false,
+      })
+    end,
   },
 }
 
